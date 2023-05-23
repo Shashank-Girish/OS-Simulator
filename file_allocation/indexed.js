10f3cf4ea6f8ac08e9d518a10ebaad2e4f07d085
Shashank-Girish <shashankg.211cs256@nitk.edu.in>
2023-05-23 22:08:38 +0530

    Basic Index allocation html

--- file_allocation/indexed.js
+++ file_allocation/indexed.js
@@ -1,128 +1,163 @@
 class IndexNode {
-    constructor(blockNumber) {
-      this.blockNumber = blockNumber;
-      this.next = null;
-    }
+  constructor(blockNumber) {
+    this.blockNumber = blockNumber;
+    this.next = null;
   }
-  
-  class FileNode {
-    constructor(name, size) {
-      this.name = name;
-      this.size = size;
-      this.index = null;
-      this.next = null;
-    }
+}
+
+class FileNode {
+  constructor(name, size) {
+    this.name = name;
+    this.size = size;
+    this.index = null;
+    this.next = null;
   }
-  
-  class LinkedList {
-    constructor(diskSize) {
-      this.head = null;
-      this.diskSize = diskSize;
-      this.usedSpace = 0;
+}
+
+class LinkedList {
+  constructor(diskSize) {
+    this.head = null;
+    this.diskSize = diskSize;
+    this.usedSpace = 0;
+  }
+
+  insertFile(name, size) {
+    if (this.usedSpace + size > this.diskSize) {
+      console.log(`Insufficient disk space to insert file '${name}'.`);
+      return;
     }
-  
-    insertFile(name, size) {
-      if (this.usedSpace + size > this.diskSize) {
-        console.log(`Insufficient disk space to insert file '${name}'.`);
-        return;
-      }
-  
-      const numBlocks = Math.ceil(size / 4); // Each block has a size of 4 units
-  
-      const indexList = new LinkedList(numBlocks);
-      for (let i = 0; i < numBlocks; i++) {
-        indexList.insertIndex(i);
-      }
-  
-      const newNode = new FileNode(name, size);
-      newNode.index = indexList;
-  
-      if (!this.head) {
-        this.head = newNode;
-      } else {
-        let currentNode = this.head;
-        while (currentNode.next) {
-          currentNode = currentNode.next;
-        }
-        currentNode.next = newNode;
-      }
-  
-      this.usedSpace += size;
-      console.log(`Inserted file '${name}' with size ${size}.`);
+
+    const numBlocks = Math.ceil(size / 4); // Each block has a size of 4 units
+
+    const indexList = new LinkedList(numBlocks);
+    for (let i = 0; i < numBlocks; i++) {
+      indexList.insertIndex(i);
     }
-  
-    insertIndex(blockNumber) {
-      const newNode = new IndexNode(blockNumber);
-  
-      if (!this.head) {
-        this.head = newNode;
-      } else {
-        let currentNode = this.head;
-        while (currentNode.next) {
-          currentNode = currentNode.next;
-        }
-        currentNode.next = newNode;
-      }
-    }
-  
-    allocateFile(name, size) {
-      if (this.usedSpace + size > this.diskSize) {
-        console.log(`Insufficient disk space to allocate file '${name}'.`);
-        return;
-      }
-  
-      const numBlocks = Math.ceil(size / 4); // Each block has a size of 4 units
-  
-      const indexList = new LinkedList(numBlocks);
-      for (let i = 0; i < numBlocks; i++) {
-        indexList.insertIndex(i);
-      }
-  
-      const newNode = new FileNode(name, size);
-      newNode.index = indexList;
-  
-      if (!this.head) {
-        this.head = newNode;
-      } else {
-        let currentNode = this.head;
-        while (currentNode.next) {
-          currentNode = currentNode.next;
-        }
-        currentNode.next = newNode;
-      }
-  
-      this.usedSpace += size;
-      console.log(`Allocated file '${name}' with size ${size}.`);
-    }
-  
-    displayFiles() {
+
+    const newNode = new FileNode(name, size);
+    newNode.index = indexList;
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
       let currentNode = this.head;
-      while (currentNode) {
-        console.log(`File: ${currentNode.name}, Size: ${currentNode.size}`);
-        console.log(`Index Blocks:`);
-        let indexNode = currentNode.index.head;
-        while (indexNode) {
-          console.log(`Block Number: ${indexNode.blockNumber}`);
-          indexNode = indexNode.next;
-        }
+      while (currentNode.next) {
         currentNode = currentNode.next;
       }
+      currentNode.next = newNode;
+    }
+
+    this.usedSpace += size;
+    console.log(`Inserted file '${name}' with size ${size}.`);
+  }
+
+  insertIndex(blockNumber) {
+    const newNode = new IndexNode(blockNumber);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
     }
   }
+
+  allocateFile(name, size) {
+    if (this.usedSpace + size > this.diskSize) {
+      console.log(`Insufficient disk space to allocate file '${name}'.`);
+      return;
+    }
+
+    const numBlocks = Math.ceil(size / 4); // Each block has a size of 4 units
+
+    const indexList = new LinkedList(numBlocks);
+    for (let i = 0; i < numBlocks; i++) {
+      indexList.insertIndex(i);
+    }
+
+    const newNode = new FileNode(name, size);
+    newNode.index = indexList;
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+
+    this.usedSpace += size;
+    console.log(`Allocated file '${name}' with size ${size}.`);
+  }
+
   
-  // Usage example:
-  const diskSize = 50; // Total disk size of 50 units
-  const fileList = new LinkedList(diskSize);
-  
-  fileList.insertFile("file1.txt", 10);
-  fileList.insertFile("file2.txt", 20);
-  fileList.insertFile("file3.txt", 15);
-  
+  displayFiles() {
+    const fileStructureDiv = document.getElementById("file-structure");
+    fileStructureDiv.innerHTML = ""; // Clear the previous file structure
+
+    let currentNode = this.head;
+    while (currentNode) {
+      const fileDiv = document.createElement("div");
+      fileDiv.textContent = `File: ${currentNode.name}, Size: ${currentNode.size}`;
+      fileDiv.classList.add("file");
+
+      const indexBlocksDiv = document.createElement("div");
+      indexBlocksDiv.textContent = "Index Blocks:";
+      indexBlocksDiv.classList.add("index-blocks");
+
+      let indexNode = currentNode.index.head;
+      while (indexNode) {
+        const blockNumberDiv = document.createElement("div");
+        blockNumberDiv.textContent = `Block Number: ${indexNode.blockNumber}`;
+        blockNumberDiv.classList.add("block-number");
+
+        indexBlocksDiv.appendChild(blockNumberDiv);
+        indexNode = indexNode.next;
+      }
+
+      fileDiv.appendChild(indexBlocksDiv);
+      fileStructureDiv.appendChild(fileDiv);
+
+      currentNode = currentNode.next;
+    }
+    }
+}
+
+// Function to handle form submission
+function insertFile() {
+  const fileNameInput = document.getElementById("file-name");
+  const fileSizeInput = document.getElementById("file-size");
+
+  const fileName = fileNameInput.value;
+  const fileSize = parseInt(fileSizeInput.value);
+
+  fileList.insertFile(fileName, fileSize);
+
+  fileNameInput.value = "";
+  fileSizeInput.value = "";
+}
+
+// Function to handle form submission
+function allocateFile() {
+  const fileNameInput = document.getElementById("file-name");
+  const fileSizeInput = document.getElementById("file-size");
+
+  const fileName = fileNameInput.value;
+  const fileSize = parseInt(fileSizeInput.value);
+
+  fileList.allocateFile(fileName, fileSize);
+
+  fileNameInput.value = "";
+  fileSizeInput.value = "";
+}
+
+// Display the file structure
+function displayFiles() {
   fileList.displayFiles();
-  
-  fileList.allocateFile("file4.txt", 12);
-  fileList.allocateFile("file2.txt", 20);
-  fileList.allocateFile("file5.txt", 25); // This file will not fit due to insufficient disk space
-  
-  fileList.displayFiles();
-  
+}
+
